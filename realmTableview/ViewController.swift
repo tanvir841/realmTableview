479474c6ab4d72779537bcdd27b026f525e235ea
Abdullah Al Numan <ab>
2020-01-04 22:18:10 -0800

    Initial Commit

---
+++ realmTableview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  realmTableview
+//
+//  Created by Tanvir on 1/4/20.
+//  Copyright © 2020 tanvir841. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
